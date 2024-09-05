084c92ca3862c83cff27461d5e3ab7ccbb3520d0
Sebastian Bogado <seebogado@gmail.com>
2024-09-05 10:23:56 -0300

    Add argument transformation and refactor PostgreSQL select feature
    Introduce `Argument` type and transformation utilities to handle arguments in the workflow. Refactor the PostgreSQL select feature to use `sqlx` and better error handling. Also, create custom error types for more precise error reporting.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nanicienta/nani-engine
 go 1.23.0
 
 require (
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.10.9
 	github.com/nanicienta/nani-commons v1.0.0
 )
--- pkg/features/postgresql/select.go
+++ pkg/features/postgresql/select.go
@@ -1,24 +1,25 @@
 package postgresql
 
 import (
-	"database/sql"
 	"fmt"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	featureConstants "github.com/nanicienta/nani-commons/pkg/constants/features"
+	"github.com/nanicienta/nani-commons/pkg/constants/features"
 	"github.com/nanicienta/nani-commons/pkg/constants/field_id"
 	"github.com/nanicienta/nani-commons/pkg/model"
+	"github.com/nanicienta/nani-commons/pkg/model/connections"
 	"github.com/nanicienta/nani-engine/internal/services"
 	"log"
 	"sync"
 	"time"
 )
 
-type PostgresSelectFeature struct {
-}
+type PostgresSelectFeature struct{}
 
 var instance *PostgresSelectFeature
 var once sync.Once
 
+// InitPostgresSelectFeature initializes the PostgresSelectFeature structure
 func InitPostgresSelectFeature() *PostgresSelectFeature {
 	once.Do(
 		func() {
@@ -26,15 +27,18 @@ func InitPostgresSelectFeature() *PostgresSelectFeature {
 			fmt.Println("Initializing postgres select feature")
 		},
 	)
+
 	engine := services.GetNaniExecutorInstance()
 	engine.RegisterFeature(instance)
 	return instance
 }
 
+// GetInternalName returns the feature name
 func (q *PostgresSelectFeature) GetInternalName() string {
-	return featureConstants.PostgresSelect
+	return features.PostgresSelect
 }
 
+// Execute runs the query and processes the result
 func (q *PostgresSelectFeature) Execute(
 	node model.Node,
 	workflow model.Workflow,
@@ -42,90 +46,152 @@ func (q *PostgresSelectFeature) Execute(
 ) (map[string]interface{}, string, error) {
 	start := time.Now()
 
-	found, queryField := node.GetField(field_id.PostgresQueryField)
-	if !found {
-		return nil, "", fmt.Errorf("query field not found")
-	}
-
-	found, connection := workflow.GetConnection(node.ConnectionId)
-	if !found {
-		return nil, "", fmt.Errorf("connection not found")
-	}
-
-	connectionString := fmt.Sprintf(
-		`user=%s dbname=%s sslmode=%s password=%s`,
-		connection.User, connection.Database,
-		connection.Ssl, connection.Password,
-	)
-
-	db, err := sql.Open("postgres", connectionString)
+	queryField, err := q.getQueryField(node)
 	if err != nil {
-		log.Fatalf("Error abriendo la conexión: %v", err)
+		return nil, "", err
+	}
+
+	connection, err := q.getConnection(workflow, node)
+	if err != nil {
+		return nil, "", err
+	}
+
+	connectionString := q.buildConnectionString(connection)
+	db, err := q.connectToDatabase(connectionString)
+	if err != nil {
+		return nil, "", err
 	}
 	defer db.Close()
 
-	rows, err := db.Query(queryField.Value)
+	rows, err := q.executeQuery(db, queryField.Value, make(map[string]interface{}))
 	if err != nil {
-		log.Fatalf("Error ejecutando la consulta: %v", err)
+		return nil, "", err
 	}
 	defer rows.Close()
 
-	columns, err := rows.Columns()
+	results, err := q.processRows(rows)
 	if err != nil {
-		log.Fatalf("Error obteniendo las columnas: %v", err)
-	}
-
-	var results []map[string]interface{}
-
-	for rows.Next() {
-		// Crear un contenedor para cada fila
-		row := make(map[string]interface{})
-
-		// Crear un slice de interfaces para las columnas
-		values := make([]interface{}, len(columns))
-		valuePtrs := make([]interface{}, len(columns))
-
-		// Asignar los punteros
-		for i := range columns {
-			valuePtrs[i] = &values[i]
-		}
-
-		// Escanear la fila actual en los punteros
-		err = rows.Scan(valuePtrs...)
-		if err != nil {
-			log.Fatalf("Error escaneando la fila: %v", err)
-		}
-
-		// Copiar los valores en el mapa de la fila
-		for i, col := range columns {
-			var v interface{}
-			val := values[i]
-			b, ok := val.([]byte)
-			if ok {
-				v = string(b)
-			} else {
-				v = val
-			}
-			row[col] = v
-		}
-
-		// Añadir la fila a los resultados
-		results = append(results, row)
-	}
-
-	if err = rows.Err(); err != nil {
-		log.Fatalf("Error en la lista de filas: %v", err)
+		return nil, "", err
 	}
 
 	elapsed := time.Since(start)
 	fmt.Printf("La consulta tomó %s\n", elapsed)
 
+	q.storeResultsInVariable(workflow, node, results)
+
+	return map[string]interface{}{"results": results}, node.Links[0].NodeId, nil
+}
+
+func (q *PostgresSelectFeature) getQueryField(node model.Node) (*model.Field, error) {
+	found, queryField := node.GetField(field_id.PostgresQueryField)
+	if !found {
+		return nil, fmt.Errorf("query field not found")
+	}
+	return &queryField, nil
+}
+
+func (q *PostgresSelectFeature) getConnection(
+	workflow model.Workflow,
+	node model.Node,
+) (*connections.BaseConnection, error) {
+	found, connection := workflow.GetConnection(node.ConnectionId)
+	if !found {
+		return nil, fmt.Errorf("connection not found")
+	}
+	return &connection, nil
+}
+
+func (q *PostgresSelectFeature) buildConnectionString(connection *connections.BaseConnection) string {
+	return fmt.Sprintf(
+		`user=%s dbname=%s sslmode=%s password=%s`,
+		connection.User, connection.Database,
+		connection.Ssl, connection.Password,
+	)
+}
+
+func (q *PostgresSelectFeature) connectToDatabase(connectionString string) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("error abriendo la conexión: %v", err)
+	}
+	return db, nil
+}
+
+func (q *PostgresSelectFeature) executeQuery(
+	db *sqlx.DB,
+	query string,
+	arguments map[string]interface{},
+) (*sqlx.Rows, error) {
+	rows, err := db.NamedQuery(query, arguments)
+	if err != nil {
+		return nil, fmt.Errorf("error ejecutando la consulta: %v", err)
+	}
+	return rows, nil
+}
+
+func (q *PostgresSelectFeature) processRows(rows *sqlx.Rows) ([]map[string]interface{}, error) {
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("error obteniendo las columnas: %v", err)
+	}
+
+	var results []map[string]interface{}
+
+	for rows.Next() {
+		row, err := q.processRow(columns, rows)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, row)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("error en la lista de filas: %v", err)
+	}
+
+	return results, nil
+}
+
+func (q *PostgresSelectFeature) processRow(
+	columns []string,
+	rows *sqlx.Rows,
+) (map[string]interface{}, error) {
+	values := make([]interface{}, len(columns))
+	valuePtrs := make([]interface{}, len(columns))
+	for i := range columns {
+		valuePtrs[i] = &values[i]
+	}
+
+	err := rows.Scan(valuePtrs...)
+	if err != nil {
+		return nil, fmt.Errorf("error escaneando la fila: %v", err)
+	}
+
+	row := make(map[string]interface{})
+	for i, col := range columns {
+		var v interface{}
+		val := values[i]
+		b, ok := val.([]byte)
+		if ok {
+			v = string(b)
+		} else {
+			v = val
+		}
+		row[col] = v
+	}
+
+	return row, nil
+}
+
+func (q *PostgresSelectFeature) storeResultsInVariable(
+	workflow model.Workflow,
+	node model.Node,
+	results []map[string]interface{},
+) {
 	if node.StoreResultInVariable {
 		if node.VariableName == "" {
-			log.Fatalf("Error. Hay que especificar el nombre de la variable: %v")
+			log.Fatalf("Error. Hay que especificar el nombre de la variable")
 		}
 		workflow.Context[node.VariableName] = results
 	}
-
-	return map[string]interface{}{"results": results}, node.Links[0].NodeId, nil
 }
